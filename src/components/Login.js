1af51f5f33ff98f39a79f67d3dc76d42a48b76d7
jzavier <jztimm@gmail.com>
2022-01-22 00:20:24 -0500

    Added content to home section

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,10 @@ const Login = (props) => {
                 </div>
             </Nav>
             <Section>
-                Section
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img src="/images/login-hero.svg" alt="" />
+                </Hero>
             </Section>
         </Container>
     )
@@ -88,9 +91,42 @@ const Section = styled.section`
     max-width: 1128px;
     align-items: center;
     margin: auto;
-    @media (max-width: 768) {
+    @media (max-width: 768px) {
         margin: auto;
         min-height: 0px;
+    }
+`;
+
+const Hero = styled.div`
+    width: 100%;
+    h1 {
+        padding-bottom: 0;
+        width: 55%;
+        font-size: 56px;
+        color: #2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width: 768px) {
+            text-align: center;
+            font-size: 20px;
+            width: 100%;
+            line-height: 2;
+        }
+    }
+
+    img {
+        /* z-index: -1; */
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+        @media (max-width: 768px) {
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
     }
 `;
 
