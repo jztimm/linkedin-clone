fa988b3a3c39d8978c993bfecd28e1270642f2c5
jzavier <jztimm@gmail.com>
2022-01-23 23:51:28 -0500

    Creating Home Screen section structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -58,6 +58,19 @@ const Section = styled.section`
     }
 `;
 
-const Layout = styled.div``;
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside main rightside";
+    grid-template-columns: minmax(0 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    /* grid-template-row: auto; */
+    margin: 25px 0;
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding: 0 5px;
+    }
+`;
 
 export default Home;
